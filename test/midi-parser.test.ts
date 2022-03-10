4063d30245e71b1acf9e59750bede1489d07c6cc
Viktor Pavlov <viktor.pavlov@bolt.eu>
2022-03-10 21:03:55 +0200

    add ci

--- test/midi-parser.test.ts
+++ test/midi-parser.test.ts
@@ -7,12 +7,22 @@ import { Midi } from "../src/parser/Parser";
 describe("Midi Parser tests", () => {
     const parser: Midi.Parser = new MidiParser();
 
+    test("parse midi file with single track", () => {
+        const buffer = readFileSync(path.join(__dirname, './data/chpn_op10_e01_format0.midi'));
+        
+        const { format, ntrks, tracks } = parser.parse(buffer);
+        
+        expect(format).toEqual(Midi.FileFormat.SINGLE_TRACK);
+        expect(ntrks).toEqual(1);
+        expect(tracks.length).toEqual(1);
+    });
+
     test("parse midi file with 3 tracks", () => {
         const buffer = readFileSync(path.join(__dirname, './data/invent2.midi'));
 
         const { format, ntrks, tracks } = parser.parse(buffer);
 
-        expect(format).toEqual(1);
+        expect(format).toEqual(Midi.FileFormat.MULTI_TRACK_SYNC);
         expect(ntrks).toEqual(3);
         expect(tracks.length).toEqual(3);
     });
