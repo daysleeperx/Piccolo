565de1c8c379b4b301be604ff067a244b0069586
Viktor Pavlov <viktor.pavlov@bolt.eu>
2022-03-10 22:22:31 +0200

    run only ts tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type {Config} from '@jest/types';
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
-  preset: "ts-jest/presets/js-with-ts"
+  preset: "ts-jest/presets/js-with-ts",
+  testMatch: [
+    '**/test/**/*.test.(ts)'
+  ]
 };
 export default config;
