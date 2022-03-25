d050c80b315ae9e81ddbcfc8b9ae4e907fbf67ad
Viktor Pavlov <viktor.pavlov@bolt.eu>
2022-03-25 15:19:53 +0200

    add commander

--- src/cli.ts
+++ src/cli.ts
@@ -4,6 +4,7 @@ import { prompt } from 'enquirer';
 import ora from 'ora';
 import figlet from 'figlet';
 import { Presets, SingleBar } from 'cli-progress'
+import { Command } from 'commander';
 import { Midi } from './parser/Parser';
 import { MidiParser } from './parser/MidiParser';
 import MidiBuilder from './parser/MidiBuilder';
@@ -134,42 +135,64 @@ async function generateAsciiArt() {
   });
 }
 
+function initCommander(): Command {
+  const program: Command = new Command();
+
+  program
+    .name('music-generator')
+    .option('-i, --source <value>')
+    .option('-o, --output <value>')
+    .option('-n, --name <value>')
+    .option('-nn, --outputsNum <value>')
+    .option('-s, --steps <value>')
+    .option('-r, --order <value>')
+    .parse(process.argv);
+  
+  return program;
+}
 async function main() {
   /* eslint-disable no-console */
   console.log(await generateAsciiArt());
+  const program: Command = initCommander();
+  let options: CLIOptions;
 
-  const options = await prompt<CLIOptions>([
-    {
-      type: 'input',
-      name: 'source',
-      message: 'Enter source MIDI file path (relative):',
-    },
-    {
-      type: 'input',
-      name: 'output',
-      message: 'Enter output direction path (relative):',
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Enter the name of output file(s):',
-    },
-    {
-      type: 'input',
-      name: 'outputsNum',
-      message: 'Enter the number of outputs:',
-    },
-    {
-      type: 'input',
-      name: 'steps',
-      message: 'Enter the number of steps to be generated:',
-    },
-    {
-      type: 'input',
-      name: 'order',
-      message: 'Enter the order of Markov chain:',
-    },
-  ]);
+  if (Object.keys(program.opts()).length === 0) {
+    options = await prompt<CLIOptions>([
+      {
+        type: 'input',
+        name: 'source',
+        message: 'Enter source MIDI file path (relative):',
+      },
+      {
+        type: 'input',
+        name: 'output',
+        message: 'Enter output direction path (relative):',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of output file(s):',
+      },
+      {
+        type: 'input',
+        name: 'outputsNum',
+        message: 'Enter the number of outputs:',
+      },
+      {
+        type: 'input',
+        name: 'steps',
+        message: 'Enter the number of steps to be generated:',
+      },
+      {
+        type: 'input',
+        name: 'order',
+        message: 'Enter the order of Markov chain:',
+      },
+    ]);
+  } else {
+    options = program.opts() as CLIOptions;
+    console.log(options);
+  }
 
   const { steps, order } = options;
 
