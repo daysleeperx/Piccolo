dd6e88c91a17e58fcf56fc79933055e5182afc99
Viktor Pavlov <viktor.pavlov@bolt.eu>
2022-04-02 16:02:27 +0300

    sync with beat loop - optimize

--- sonic_pi/osc_endpoint.rb
+++ sonic_pi/osc_endpoint.rb
@@ -16,21 +16,20 @@ end
 live_loop :play_gen_sequence, sync: :beat do
   gen_notes = get[:sequence] || []
   gen_steps = get[:steps] || []
-  notes = gen_notes.zip(gen_steps).ring
+  notes = gen_notes.zip(gen_steps)
   puts notes
   
   if notes.empty?
     sleep 1
   else
-    loop do
-      note, step = notes.tick
+    notes.each do |note, step|
       play note, release: 0.1
       sleep step
     end
   end
 end
 
-live_loop :play_note, sync: :beat do
-  play choose(chord(:E3, :minor)), release: 0.1, cutoff: rrand(60, 120), amp: 0.7
+live_loop :rhytm, sync: :beat do
+  play 64, release: 0.2, amp: 0
   sleep 0.25
 end
