15d2374d11b8a5c31a94ee0c0383d4ccb9f00895
Viktor Pavlov <viktor.pavlov@bolt.eu>
2022-08-31 00:13:04 +0300

    add bass line

--- sonic_pi/haim.rb
+++ sonic_pi/haim.rb
@@ -19,6 +19,27 @@ live_loop :snare, sync: :metronome do
 end
 
 live_loop :hihat, sync: :metronome do
-  sample :drum_cymbal_closed if pattern "x-x-x-x-x-x-x-x-"
-  sleep 0.25
+  sample :drum_cymbal_closed
+  sleep 0.5
+end
+
+bass_line = [
+  [:E1, 1.75], 
+  [:Ds2, 0.25], 
+  [:E1, 0.25], 
+  [:r, 0.25], 
+  [:E1, 0.5], 
+  [:Ds2, 0.5], 
+  [:E1, 0.5], 
+  [:B1, 3],
+  [:Fs1, 0.5],
+  [:Bb1, 0.25],
+  [:B1, 0.25],
+].ring
+
+live_loop :bass, sync: :metronome do
+  use_synth :saw
+  note, step = bass_line.tick
+  play note, cutoff: 50, sustain: step, release: 0
+  sleep step
 end
