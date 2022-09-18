af516d981da3f700c1847916d8f8c41a4dea54bc
Viktor Pavlov <viktor.pavlov@bolt.eu>
2022-09-18 20:54:10 +0300

    clean up

--- sonic_pi/haim.rb
+++ sonic_pi/haim.rb
@@ -1,5 +1,9 @@
+# "Summer Girl" by HAIM
+# Coded by Viktor Pavlov
+
 use_bpm 100
 
+# Globals
 set :bass_line, [
   [:E2, 1.75],
   [:Ds3, 0.25],
@@ -12,13 +16,6 @@ set :bass_line, [
   [:Fs2, 0.5],
   [:Bb2, 0.25],
   [:B2, 0.25],
-]
-
-set :slide_bass, [
-  [:E1, 3.75],
-  [:E2, 0.25],
-  [:B1, 3.75],
-  [:E2, 0.25],
 ]
 
 set :sax_part, [
@@ -68,6 +65,34 @@ set :sax_part, [
   [51, 4],
 ]
 
+# Functions
+define :pattern do |pattern|
+  return pattern.ring.tick == "x"
+end
+
+define :sax_synth_defaults do |step| 
+  return {
+    cutoff: 96, 
+    amp: 1, 
+    attack: step > 2 ? step * 0.1 : 0.2, 
+    attack_level: 1, 
+    decay: step * 0.1, 
+    sustain: step * 0.3, 
+    sustain_level: 0.7, 
+    release: step > 1 ? step * 0.4 : step * 0.2
+  }
+end
+
+# Metronome
+live_loop :met_whole do
+  sleep 4
+end
+
+live_loop :met_q, sync: :met_whole do
+  sleep 1
+end
+
+# Loops
 live_loop :receive_sax_part do
   use_real_time
   seq = sync "/osc*/gen/sequence"
@@ -75,65 +100,38 @@ live_loop :receive_sax_part do
   set :sax_part, seq.zip(s)
 end
 
-live_loop :metronome do
-  sleep 1
-end
-
-live_loop :sax_start, sync: :metronome do
-  sleep 4
-end
-
-define :pattern do |pattern|
-  return pattern.ring.tick == "x"
-end
-
-live_loop :percussion, sync: :sax_start do
-  sample :tabla_ghe1, amp: 1, rate: 1.2  if pattern "x-x----x-x--x---"
-  sleep 0.25
-end
-
-live_loop :kick, sync: :metronome do
+live_loop :kick, sync: :met_q do
   sample :drum_heavy_kick if pattern "x-x----x---x-x--"
   sleep 0.25
 end
 
-live_loop :snare, sync: :metronome do
+live_loop :snare, sync: :met_q do
   sample :drum_snare_hard if pattern "----x----x--x---"
   sleep 0.25
 end
 
-live_loop :hihat, sync: :metronome do
+live_loop :hihat, sync: :met_q do
   sample :drum_cymbal_closed
   sleep 0.5
 end
 
-live_loop :bass, sync: :sax_start do
+live_loop :percussion, sync: :met_whole do
+  stop
+  sample :tabla_ghe1, amp: 1, rate: 1.2  if pattern "x-x----x-x--x---"
+  sleep 0.25
+end
+
+live_loop :bass, sync: :met_whole do
+  stop
   use_synth :saw
   note, step = get[:bass_line].tick
   play note, amp: 2, cutoff: 50, attack: 0, decay: step * 0.5, sustain: 0, release: step * 0.7
   sleep step
 end
 
-live_loop :sl_bass, sync: :sax_start do
-   use_synth :saw
-   note, step = get[:slide_bass].tick
-   play note, amp: 2, cutoff: 60, attack: 0, release: step
-   sleep step
-end
-
-# live_loop :midi_moog, sync: :metronome do
-#   note, step = bass_line.tick
-#   midi_note_on note, channel: 0
-#   sleep step
-#   midi_note_off note, channel: 0
-# end
-#
-# midi_all_notes_off
-
-# use_bpm 100
-
 with_fx :reverb, room: 0.9 do
-  live_loop :sax, sync: :sax_start do
+  live_loop :sax, sync: :met_whole do
+    stop
     notes = get[:sax_part] || []
     puts notes
 
@@ -142,8 +140,8 @@ with_fx :reverb, room: 0.9 do
     else
       notes.each do |note, step|
         use_synth :saw
-        note, step = get[:sax_part].tick
-        play note, cutoff: 96, amp: 0, attack: step > 2 ? step * 0.1 : 0.2, attack_level: 1, decay: step * 0.1, sustain: step * 0.3, sustain_level: 0.7, release: step > 1 ? step * 0.4 : step * 0.2
+        use_synth_defaults sax_synth_defaults step
+        play note 
         sleep step
       end
     end
@@ -153,4 +151,12 @@ end
 use_osc "localhost", 9912
 osc "/gen/sequence", *get[:sax_part].to_json
 
+# live_loop :midi_moog, sync: :met_q do
+#   note, step = bass_line.tick
+#   midi_note_on note, channel: 0
+#   sleep step
+#   midi_note_off note, channel: 0
+# end
+#
+# midi_all_notes_off
 
--- sonic_pi/sequential_demo.rb
+++
@@ -1,8 +0,0 @@
-live_loop :foo2 do
-    with_fx :reverb, mix: 0.2, room: 0.8 do
-      use_synth :fm
-      use_real_time
-      note = sync "/osc*/melody/notes"
-      play note
-    end
-  end
